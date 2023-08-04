88ba2230939811c531002a738033df810d2bde0c
Matan U <dev.muchen@gmail.com>
2023-08-04 11:00:51 -0400

    committing for submission

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const crypto = require("node:crypto");
+const stdout = require("process").stdout;
 
 const BASE_URL = `https://recruitment-test.investcloud.com`;
 
@@ -24,51 +25,107 @@ async function buildMatrix(dataLabel, size) {
 
   for (let i = 0; i < size; i++) {
     let row = (await getDataSet(dataLabel, "row", i)).data.Value;
-    matrix[i] = [...row]; //
+    matrix[i] = [...row];
   }
 
   return matrix;
 }
 
 async function main(size = 1000) {
-  await initDataSets(size);
-  const matrixA = await buildMatrix("A", size);
-  const matrixB = await buildMatrix("B", size);
-  // const multiMatrix = multiplyMatrix(matrixA, matrixB);
-  const resultMatrix = new Array(size)
-    .fill(0)
-    .map(() => new Array(size).fill(0));
-  let hash = "";
+  try {
+    await initDataSets(size);
 
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      for (let k = 0; k < size; k++) {
-        resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+    return Promise.allSettled([
+      buildMatrix("A", size),
+      buildMatrix("B", size),
+    ]).then(async (result) => {
+      // we got both of em
+      if (result.every((r) => r.status === "fulfilled")) {
+        const matrixA = result[0].value,
+          matrixB = result[1].value;
+        const resultMatrix = new Array(size)
+          .fill(0)
+          .map(() => new Array(size).fill(0));
+
+        // working algorithm
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            for (let k = 0; k < size; k++) {
+              resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+          }
+        }
+
+        console.log();
+        console.log("Matrix A");
+        // matrixA.forEach((row) => console.log(...row));
+        console.log();
+        console.log("Matrix B");
+        // matrixB.forEach((row) => console.log(...row));
+        console.log();
+        console.log("Result Matrix");
+        // resultMatrix.forEach((row) => console.log(...row));
+
+        // const preEncode = resultMatrix.map((row) => row.join("")).join("");
+        const encoded = encode(
+          resultMatrix.map((row) => row.join("")).join("")
+        );
+
+        // console.log();
+        // console.log("hash pre encode");
+        // console.log(preEncode);
+        console.log();
+        console.log("hash encoded");
+        console.log(encoded);
+
+        const valid = await validate(encoded);
+        console.log(valid.data);
+        return Promise.resolve();
+      } else {
+        throw new Error("not loaded");
       }
-    }
+    });
+  } catch (e) {
+    throw new Error(e);
   }
-
-  console.log("Matrix A");
-  matrixA.forEach((row) => console.log(...row));
-  console.log();
-  console.log("Matrix B");
-  matrixB.forEach((row) => console.log(...row));
-  console.log();
-  console.log("Result Matrix");
-  resultMatrix.forEach((row) => console.log(...row));
-
-  const preEncode = resultMatrix.map((row) => row.join("")).join("");
-  const encoded = encode(resultMatrix.map((row) => row.join("")).join(""));
-
-  console.log();
-  console.log("hash pre encode");
-  console.log(preEncode);
-  console.log();
-  console.log("hash encoded");
-  console.log(encoded);
-
-  const valid = await validate(encoded);
-  console.log(valid.data);
 }
 
-main(3);
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return seconds == 60
+    ? `${minutes + 1} minutes"`
+    : `${minutes} minutes and ${seconds} seconds`;
+}
+
+// found a neat lil spinner so I included it to make it look nicer while loading
+function startSpinner() {
+  const characters = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+  const cursorEsc = {
+    hide: "\u001B[?25l",
+    show: "\u001B[?25h",
+  };
+  stdout.write(cursorEsc.hide);
+
+  let i = 0;
+  const timer = setInterval(function () {
+    stdout.write("\r" + characters[i++]);
+    i = i >= characters.length ? 0 : i;
+  }, 150);
+
+  return () => {
+    clearInterval(timer);
+    stdout.write("\r");
+    stdout.write(cursorEsc.show);
+  };
+}
+
+console.log("starting now...");
+const start = performance.now();
+const stopSpinner = startSpinner();
+
+main().then(() => {
+  const end = performance.now();
+  stopSpinner();
+  console.log("this took ", millisToMinutesAndSeconds(end - start));
+});
